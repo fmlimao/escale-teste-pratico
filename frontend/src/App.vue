6d1a19edfdd8f667d3007f526f2c2c239b797022
Leandro Macedo <fmlimao@gmail.com>
2025-09-07 09:01:34 -0300

    Deletando pokemons no frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -47,6 +47,18 @@ const fetchPokemons = async () => {
   }
 };
 
+const deletePokemon = async (id: string) => {
+  if (confirm('Tem certeza que deseja excluir este Pokemon?')) {
+    try {
+      await api.delete(id);
+      // Atualiza a lista de pokemons após a exclusão
+      fetchPokemons();
+    } catch (error) {
+      console.error('Erro ao excluir pokemon:', error);
+    }
+  }
+};
+
 onMounted(() => {
   fetchPokemons();
 });
@@ -75,7 +87,15 @@ onMounted(() => {
             <p class="id">
               <span class="number-prefix">Nº&nbsp;</span>{{ pokemon.id }}
             </p>
-            <h5>{{ pokemon.name }}</h5>
+            <div class="pokemon-name-container">
+              <h5>{{ pokemon.name }}</h5>
+              <button class="delete-button" @click.prevent="deletePokemon(pokemon._id)" title="Excluir Pokemon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff0000" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+              </button>
+            </div>
 
             <div class="abilities">
               <span v-for="type in pokemon.types" :key="type.slot">
